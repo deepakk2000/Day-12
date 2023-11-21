e7e77737c8621621236ccd4be05158d9d9eff450
deepakk2000 <deepakjc112000@gmal.com>
2023-11-21 15:35:10 +0530

    A-17

---
+++ Opps-Part1/Opps-Part1/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps_Part1
+{
+    internal class BankAccount
+    {
+        public string AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string accountHolderName)
+        {
+            AccountNumber = GenerateAccountNumber();
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited {amount:C}. Neww balance:{Balance:C}");
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds. Withdrawal faield");
+            }
+            else
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn {amount:C}.New balance:{Balance:C}");
+            }
+        }
+        private static string GenerateAccountNumber()
+        {
+            return Guid.NewGuid().ToString("N").Substring(0, 10);
+
+        }
+    }
+
+   
+}
---
+++ Opps-Part1/Opps-Part1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount("Deep");
+            myAccount.Deposit(100000);
+            myAccount.Withdraw(500);
+            Console.WriteLine($"Account Number:{myAccount.AccountNumber}");
+            Console.WriteLine($"Account Holder:{myAccount.AccountHolderName}");
+            Console.WriteLine($"Account Balance:{myAccount.Balance}");
+            Console.ReadKey();
+        }
+    }
+}
